4512a2ef0e74792d8c462c88bc69cd1b2d8509dc
Sorokin Max <sorokinmx@gmail.com>
2024-03-04 19:00:53 +0300

    ENVs fixed

--- internal/globals/globals.go
+++ internal/globals/globals.go
@@ -3,14 +3,14 @@ package globals
 // SERVICE DESCRIPTION
 const ServiceFriendlyName = "Android store"
 const ServiceName = "android-store"
-const Version = "1.1.1"
+const Version = "1.1.2"
 
 // ENVIRONMENT VARIABLES CONFIGURATION STRUCTURE
 type ConfigStruct struct {
 	HttpPort uint   `env:"AS_HTTP_PORT" envDefault:"80"`
-	Url      string `env:"AS_HTTP_PORT" envDefault:"http://localhost:80"`
-	BotToken string `env:"TELEGRAM_BOT_TOKEN" envDefault:""`
-	ChatID   int    `env:"TELEGRAM_CHAT_ID" envDefault:"0"`
+	Url      string `env:"AS_HTTP_URL" envDefault:"http://localhost:80"`
+	BotToken string `env:"AS_TELEGRAM_BOT_TOKEN" envDefault:""`
+	ChatID   int    `env:"AS_TELEGRAM_CHAT_ID" envDefault:"0"`
 }
 
 // GLOBAL VARIABLES
